454a60b8a1a9795ae624bc4d620aebfa529f51d6
unknown <muhammadfurqan0100@gmail.com>
2024-11-05 20:33:11 +0500

    Vercel Added

--- app/routers/blog.py
+++ app/routers/blog.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from .. import schemas, models, oauth
 from ..database import get_db
 from sqlalchemy.orm import Session
-from blog.repository import blog
+from app.repository import blog
 from typing import List
 router = APIRouter(
     prefix="/blog",
